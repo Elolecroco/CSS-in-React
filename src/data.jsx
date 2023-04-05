2fe1b47e7f536b12b9874cc612a134afd213cbaa
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-04-05 10:25:09 +0200

    changements mineurs

--- src/data.jsx
+++ src/data.jsx
@@ -18,7 +18,17 @@ const MovieInfo = [
         pressRating: "92%",
         imgSrc: "https://fr.web.img6.acsta.net/medias/nmedia/18/90/08/59/20366454.jpg",
         description: "Two years before the Civil War, Django (Jamie Foxx), a slave, finds himself accompanying an unorthodox German bounty hunter named Dr. King Schultz (Christoph Waltz) on a mission to capture the vicious Brittle brothers. Their mission successful, Schultz frees Django, and together they hunt the South's most-wanted criminals. Their travels take them to the infamous plantation of shady Calvin Candie (Leonardo DiCaprio), where Django's long-lost wife (Kerry Washington) is still a slave.",
-        }
+    },
+    {
+        name: "The Conjuring",
+        year: "2013",
+        genre: "Horror/Mystery",
+        duration: "1 h 51",
+        publicRating: "86%",
+        pressRating: "83%",
+        imgSrc: "https://fr.web.img6.acsta.net/pictures/210/025/21002526_20130430172022533.jpg",
+        description: "In 1970, paranormal investigators and demonologists Lorraine (Vera Farmiga) and Ed (Patrick Wilson) Warren are summoned to the home of Carolyn (Lili Taylor) and Roger (Ron Livingston) Perron. The Perrons and their five daughters have recently moved into a secluded farmhouse, where a supernatural presence has made itself known. Though the manifestations are relatively benign at first, events soon escalate in horrifying fashion, especially after the Warrens discover the house's macabre history.",
+    }
 ]
 
 export default MovieInfo
